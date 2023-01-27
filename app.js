bcb7e1b30180ce37113397a43c784f28ef9fdb31
sarabjeeth kaur <sarabjeethkaur@gmail.com>
2023-01-27 21:21:49 +0530

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello there!!Iam creating continuous delivery pipeline.'));
+app.get('/', (req, res) => res.send('Hello there!!Iam creating continuous delivery pipeline. /In here i added manual approval./'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
