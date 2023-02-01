d064acf35a76fded9c6fb779c195bab44e68ec20
sarabjeeth kaur <sarabjeethkaur@gmail.com>
2023-02-01 13:34:38 +0530

    changed on 2/1 and after deploy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello there!!Iam creating continuous delivery pipeline. /In here i added manual approval./"Redoing on 2/1"'));
+app.get('/', (req, res) => res.send('Hello there!!Iam creating continuous delivery pipeline. /In here i added manual approval./"Redoing on 2/1"\CHANGES AFTER DEPLOYING APP\'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
